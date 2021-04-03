f8d4937ac542bc1f5afd5367140eb6eabe69c378
Atom666 <lamartem865@gmail.com>
2021-04-03 21:06:38 +0300

    script.js цикл While

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("JS is working")
+console.log("JS is working");
+
+let funds = 50;
+
+while (funds > 1 && funds < 100) {
+
+    // Размещение ставок
+
+    // Бросок костей
+
+    // Получение выигрыша
+
+}
