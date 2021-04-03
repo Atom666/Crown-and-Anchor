abd26dfe38037b23812f583487785d13026dd504
Atom666 <lamartem865@gmail.com>
2021-04-03 21:27:15 +0300

    script.js add function

--- script.js
+++ script.js
@@ -2,12 +2,27 @@ console.log("JS is working");
 
 let funds = 50;
 
-while (funds > 1 && funds < 100) {
+// while (funds > 1 && funds < 100) {
 
-    // Размещение ставок
+     // Размещение ставок
 
-    // Бросок костей
+     // Бросок костей
 
-    // Получение выигрыша
+     // Получение выигрыша
 
+// }
+
+// Функция генерации рандом чисел для броска костей
+function rand(m, n){
+    return m + Math.floor((n - m + 1) * Math.random());
 }
+
+// Функция генерирующая случайным образом на какую грань упал кубик
+function randFace(){
+    return ["crown", "anchor", "heart", " spade", "club", " diamond"]
+        [rand(0, 5)];
+}
+
+
+
+
