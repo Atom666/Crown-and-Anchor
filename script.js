3c86641302fc850b0c11e22800a0d5a38705fd7f
Atom666 <lamartem865@gmail.com>
2021-04-03 21:59:12 +0300

    script.js add Получение выигрыша цикл for + объединение всех кусков кода

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ let funds = 50;
 while (funds > 1 && funds < 100) {
 
 // Размещение ставок
+
     // Функция генерации рандом чисел для броска костей
     function rand(m, n){
         return m + Math.floor((n - m + 1) * Math.random());
     }
+
 
     // Функция генерирующая случайным образом на какую грань упал кубик
     function randFace(){
@@ -16,8 +18,10 @@ while (funds > 1 && funds < 100) {
             [rand(0, 5)];
     }
 
+
     // Начальное число кол-во выпавших граней
     const bets = (crown: 0, auchor:0, heart:0, spade:0,  club:0, diamond: 0 );
+
 
     // рандомная  ставка
     let totalBet = rand(1, funds);
@@ -40,11 +44,32 @@ while (funds > 1 && funds < 100) {
         bets[face] = bets[face] + bet;
         remaining = totalBet - bet;
     } while(remaining > 0); // У словие в цикле do...while пишется в конце
+
+
+
+
      // Бросок костей
 
+     const hand = [];
+
+     for(let roll = 0; roll < 3; roll++){
+
+        hand.puss(randFace());
+
+     }
+
+
      // Получение выигрыша
-
-// }
-
+     let winnings = 0;
+     for(let die = 0; die < hand.length; die++){
+         let face = hand[die];
+         if(bets[face] > 0){
+             winnings = winnings + bets[face];
+         }
+     }
+     funds = funds + winnings;
+    
+}
+console.log(`\tending funs: ${funds}`);
 
 
