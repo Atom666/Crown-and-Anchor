b2e4d0ea260f0d3ea9f9c8e1cec98d515b66d53f
Atom666 <lamartem865@gmail.com>
2021-04-04 19:33:37 +0300

    Finish commit

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
 console.log("JS is working");
 
+
+
+
 let funds = 50;
+let round = 0;
+
 
 while (funds > 1 && funds < 100) {
-
-// Размещение ставок
-
-    // Функция генерации рандом чисел для броска костей
-    function rand(m, n){
-        return m + Math.floor((n - m + 1) * Math.random());
-    }
+    round++
+    console.log(`\nRound: ${round}`);
+    console.log(`\tStarting funds: ${funds}p`);
 
 
-    // Функция генерирующая случайным образом на какую грань упал кубик
-    function randFace(){
-        return ["crown", "anchor", "heart", " spade", "club", " diamond"]
-            [rand(0, 5)];
-    }
 
+// Размещение ставок bets
 
     // Начальное число кол-во выпавших граней
-    const bets = (crown: 0, auchor:0, heart:0, spade:0,  club:0, diamond: 0 );
-
-
+    const bets = {crown: 0, anchor:0, heart:0, spade:0,  club:0, diamond: 0 };
     // рандомная  ставка
     let totalBet = rand(1, funds);
 
@@ -31,33 +26,25 @@ while (funds > 1 && funds < 100) {
         bets.heart = totalBet;
     } else {
         // Случайное распределение всех ставок 
+        // Размещение ставок 
+        let remaning = totalBet;
+        do {
+            let bet = rand(1, remaning);
+            let face = randFace();
+            bets[face] = bets[face] + bet;
+            remaning = remaning - bet;
+        } while ( remaning > 0);
     }
     funds = funds - totalBet;
-
-
-    // Размещение ставок 
-    let remaining = totalBet; 
-
-    do{
-        let bet =  rand(1, remaining);
-        let face = randFace();
-        bets[face] = bets[face] + bet;
-        remaining = totalBet - bet;
-    } while(remaining > 0); // У словие в цикле do...while пишется в конце
-
-
+    console.log(`\tbets: ` + Object.keys(bets).map(face => `${face}: ${bets[face]} pence`).join(', ') + ` (total: ${totalBet} pence)`);
 
 
      // Бросок костей
-
      const hand = [];
-
      for(let roll = 0; roll < 3; roll++){
-
-        hand.puss(randFace());
-
+        hand.push(randFace());
      }
-
+     console.log(`\thand: ${hand.join(', ')}`);
 
      // Получение выигрыша
      let winnings = 0;
@@ -73,3 +60,14 @@ while (funds > 1 && funds < 100) {
 console.log(`\tending funs: ${funds}`);
 
 
+// Функция генерации рандом чисел для броска костей
+function rand(m, n){
+    return m + Math.floor((n - m + 1) * Math.random());
+}
+
+
+// Функция генерирующая случайным образом на какую грань упал кубик
+function randFace(){
+    return ["crown", "anchor", "heart", "spade", "club", "diamond"]
+        [rand(0, 5)];
+} 
